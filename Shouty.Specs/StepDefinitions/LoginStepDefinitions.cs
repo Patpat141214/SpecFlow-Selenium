34a84275308c37bab8c68aea17116d2102ca6476
Patrick Igi Feniza <patrickwilliamsfeniza@gmail.com>
2024-11-22 15:45:08 +0800

    WithTRX

--- Shouty.Specs/StepDefinitions/LoginStepDefinitions.cs
+++ Shouty.Specs/StepDefinitions/LoginStepDefinitions.cs
@@ -2,6 +2,7 @@ using System;
 using TechTalk.SpecFlow;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Shouty.Specs.StepDefinitions
 {
@@ -9,12 +10,14 @@ namespace Shouty.Specs.StepDefinitions
     public class LoginStepDefinitions
     {
         private IWebDriver driver;
+        WebDriver wait;
 
         [Given(@"the user is on the login page")]
         public void GivenTheUserIsOnTheLoginPage()
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://manager.easyclaimsph.com/Account/Login?ReturnUrl=%2F"); // Replace with your login page URL
+            driver.Manage().Window.Maximize();
         }
 
         [When(@"the user enters valid credentials")]
@@ -23,6 +26,7 @@ namespace Shouty.Specs.StepDefinitions
             driver.FindElement(By.Id("Username")).SendKeys("patche1214");
             driver.FindElement(By.Id("Password")).Click();
             driver.FindElement(By.Id("Password")).SendKeys("1234567");
+            Thread.Sleep(2000);
             driver.FindElement(By.Id("submitBtn")).Click();
         }
 
@@ -31,13 +35,16 @@ namespace Shouty.Specs.StepDefinitions
         {
             driver.FindElement(By.Id("Username")).SendKeys("patasdche1214");
             driver.FindElement(By.Id("Password")).SendKeys("1as2346");
+            Thread.Sleep(2000);
             driver.FindElement(By.Id("submitBtn")).Click();
+
         }
 
         [Then(@"the user should be redirected to the homepage")]
         public void ThenTheUserShouldBeRedirectedToTheHomepage()
         {
             Assert.Equal("https://manager.easyclaimsph.com/", driver.Url); // Replace with your homepage URL
+            Thread.Sleep(2000);
             driver.Quit();
         }
 
@@ -51,7 +58,7 @@ namespace Shouty.Specs.StepDefinitions
 
             // Verify the error message content
             Assert.Equal("The username and password you entered does not match or does not exist. Please try again.", errorMessage);
-
+            Thread.Sleep(2000);
             // Quit the driver
             driver.Quit();
         }
